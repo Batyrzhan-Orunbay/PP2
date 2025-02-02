399f885e894c3f174a4f53491ad57ef528729542
Batyrzhan <batyrzhan0607@gmail.com>
2025-02-02 22:58:17 +0500

    lab3

--- lab3/functions.py
+++ lab3/functions.py
@@ -115,4 +115,18 @@ histogram([4, 9, 7])
 
 
 #13
+import random
+def guess_num():
+    n = random.randint(1,21)
+    print("Hello! What is your name?")
+    name = input("")
+    print(f"Well, {name}, I am thinking of a number between 1 and 20.\nTake a guess.")
+    g = int(input())
+    while g != n:
+        if g < n:
+            print("\nYour guess is too low.\nTake a guess.")
+        else:
+            print("\nYour guess is too low.\nTake a guess.")
+        g = int(input())
+    print(f"Good job, KBTU! You guessed my number in {n} guesses!")
 
--- lab3/functions2.py
+++ lab3/functions2.py
@@ -86,4 +86,45 @@ def imdb(movies):
                 return True
     return False
 print(imdb(movies))
+
+
+#2
+def filter_movies(movies):
+    result=[]
+    for movie in movies:
+        if movie["imdb"]>5.5:
+            result.append(movie)
+    return result
+print(filter_movies(movies))
+    
+
+#3
+def samecategory(category):
+    names = []
+    for movie in movies:
+        if movie["category"] == category:
+            names.append(movie["name"])
+    return names
+print(samecategory("Romance"))
+
+
+#4
+def average():
+    a = 0
+    for movie in movies:
+        a = movie["imdb"] + a
+    return a / len(movies)
+print(average())
+
+
+#5
+def avgcategory(category):
+    avg = 0
+    s = 0
+    for i in movies:
+        if i["category"] == category:
+            avg += i['imdb']
+            s += 1
+    return avg / s
+print(avgcategory("Thriller"))
     
