ce189d609dcdfe8154902edafcb3b755c35d4dec
Batyrzhan <batyrzhan0607@gmail.com>
2025-02-02 16:46:37 +0500

    lab3

--- lab2/while.py
+++ lab2/while.py
@@ -4,3 +4,19 @@ while i < 6:
   if i == 3:
     continue
   print(i)
+  
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+  
+  
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
