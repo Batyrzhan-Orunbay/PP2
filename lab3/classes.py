00450fdb496e94a032704b509049b6eef8934e3f
Batyrzhan <batyrzhan0607@gmail.com>
2025-02-16 15:55:41 +0500

    test data

--- lab3/classes.py
+++ lab3/classes.py
@@ -1,79 +1,79 @@
 #1
-class String:
-    def getstring(self):
-        self.sentence=input("Sentense:")
-    def printstring(self):
-        print("With upper case:"+self.sentence.upper())  
-mystring=String()
-mystring.getstring()
-mystring.printstring()
+# class String:
+#     def getstring(self):
+#         self.sentence=input("Sentense:")
+#     def printstring(self):
+#         print("With upper case:"+self.sentence.upper())  
+# mystring=String()
+# mystring.getstring()
+# mystring.printstring()
 
 
-#2
-class Shape():
-    def area(self):
-        return 0
-class Square(Shape):
-    def __init__(self,length):
-        self.length=length
-    def area(self):
-        return self.length ** 2
-a = float(input("a:"))
-mysquare=Square(a)
-print("sharshy audany:",mysquare.area())
-myshape=Shape()
-print("Basqa:",myshape.area())
+# #2
+# class Shape():
+#     def area(self):
+#         return 0
+# class Square(Shape):
+#     def __init__(self,length):
+#         self.length=length
+#     def area(self):
+#         return self.length ** 2
+# a = float(input("a:"))
+# mysquare=Square(a)
+# print("sharshy audany:",mysquare.area())
+# myshape=Shape()
+# print("Basqa:",myshape.area())
 
 
-#3
-class Shape:
-    def area(self):
-        return 0
-class Rectangle():
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-    def area(self):
-        return self.length * self.width
-length = float(input("Length: "))
-width = float(input("Width: "))
-audan=Rectangle(length,width)
-print("Area of the rectangle:",audan.area())
+# #3
+# class Shape:
+#     def area(self):
+#         return 0
+# class Rectangle():
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
+#     def area(self):
+#         return self.length * self.width
+# length = float(input("Length: "))
+# width = float(input("Width: "))
+# audan=Rectangle(length,width)
+# print("Area of the rectangle:",audan.area())
 
 
-#4
-import math
+# #4
+# import math
 
-class Point:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self,x,y):
+#         self.x = x
+#         self.y = y
 
-    def show(self):
-        print(f"Coordinates: ({self.x}, {self.y})")
+#     def show(self):
+#         print(f"Coordinates: ({self.x}, {self.y})")
 
-    def move(self, x1, y1):
-        self.x = x1
-        self.y = y1
+#     def move(self, x1, y1):
+#         self.x = x1
+#         self.y = y1
 
-    def dist(self, point2):
-        dx = self.x - point2.x
-        dy = self.y - point2.y
-        distance = math.sqrt(dx**2 + dy**2)
-        return distance
-a=float(input("x:"))
-b=float(input("y:"))
-point1 = Point(a,b)
-point2 = Point(a,b)
+#     def dist(self, point2):
+#         dx = self.x - point2.x
+#         dy = self.y - point2.y
+#         distance = math.sqrt(dx**2 + dy**2)
+#         return distance
+# a=float(input("x:"))
+# b=float(input("y:"))
+# point1 = Point(a,b)
+# point2 = Point(a,b)
 
-point1.show() 
-k=float(input("x1:"))
-p=float(input("y1:"))
-point1.move(k,p)
-point1.show() 
+# point1.show() 
+# k=float(input("x1:"))
+# p=float(input("y1:"))
+# point1.move(k,p)
+# point1.show() 
 
-distance = point1.dist(point2)
-print(f"Distance between point1 and point2: {distance}")
+# distance = point1.dist(point2)
+# print(f"Distance between point1 and point2: {distance}")
 
 
 #5
@@ -102,7 +102,7 @@ class Bank():
             return f"you're balance: {self.money},  and you take {money}"
         
         
-bank = Bank("Bekzat", 1000)
+bank = Bank("Batyrzhan", 1000)
 
 print(bank.balance())
 
