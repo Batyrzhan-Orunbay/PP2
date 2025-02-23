47c7449d5028b09d76002536b70ba37f8c8a9e31
Batyrzhan <batyrzhan0607@gmail.com>
2025-02-23 23:45:48 +0500

    lab5

--- lab4/math.py
+++ lab4/math.py
@@ -3,7 +3,7 @@ import math
 def degree_to_radian(d):
     return d * (math.pi/180)
 d=int(input("Degree: "))
-print(degree_to_radian(d))
+print("Radian: ", degree_to_radian(d))
 
 #2
 import math
@@ -12,7 +12,7 @@ def area(a,b,h):
 h=int(input("Height: "))
 a=int(input("Base, first value: "))
 b=int(input("Base, second value: "))
-print(area(a,b,h))
+print("Expected Output: ", area(a,b,h))
 
 #3
 from math import tan,pi
@@ -20,7 +20,7 @@ def area(s,l):
     return s * (l**2) / (4*tan(pi/s))
 s=int(input("Input number of sides: "))
 l=int(input("Input the length of a side: "))
-print(area(s,l))
+print("The area of the polygon is: ", area(s,l))
 
 #4
 import math
@@ -28,4 +28,4 @@ def area(a,h):
     return a * h
 a=int(input("Length of base: "))
 h=int(input("Height of parallelogram: "))
-print(area(a,h))
+print("Expected Output: ", area(a,h))
--- lab5/regex.py
+++ lab5/regex.py
@@ -1,25 +1,53 @@
-#1
 import re
-pattern = r'a*b*'
-test = ["a", "ab", "abb", "aab", "b", "xyz"]
-for s in test:
-    if re.fullmatch(pattern,s):
-        print(f"Match: {s}")
 
+with open('row.txt', "r", encoding='utf-8') as file:
+    txt = file.read()
+
+
+#1
+pattern = re.findall(r'ab{0,}', txt)
+print(pattern)
 
 #2
-import re
-pattern = r'ab{2,3}'
-test = ["abb", "abbb", "ab", "a", "abbbb"]
-for s in test:
-    if re.fullmatch(pattern, s):
-        print(f"Match: {s}")
-        
-        
+pattern = re.findall(r'ab{2,3}', txt)
+print(pattern)
+
 #3
-import re
-pattern = r'[a-z]+_[a-z]+'
-test = ["hello_world", "helloWorld", "hello_world_test", "test_", "_test"]
-for s in test:
-    if re.fullmatch(pattern, s):
-        print(f"Match: {s}")
+pattern = re.findall(r'[a-z]*+_[a-z]*', txt)
+print(pattern)
+
+#4
+pattern = re.findall(r'[A-Z]{1}+[a-z]+', txt)
+print(pattern)
+
+#5
+pattern = re.findall(r'a.*?b', txt)
+print(pattern)
+
+#6
+pattern = re.sub(r'[ ,.]', ':', txt)
+print(pattern)
+
+#7
+pattern = re.findall(r'[A-Za-z]+_[A-Za-z]+', txt)
+cc = []
+for i in pattern:
+    a = i.split('_')
+    cc.append(a[0]+''.join(x.capitalize() for x in a[1:]))
+print(cc)
+
+#8
+pattern = re.findall(r'[A-Z][^A-Z]+', txt)
+print(pattern)
+
+#9
+pattern = re.sub(r'(?<!^)(?=[A-Z])', ' ', txt)
+print(pattern)
+
+#10
+pattern = re.findall(r'[A-Za-z]+[A-Z]+', txt)
+cc = []
+for i in pattern:
+    cc.append(re.sub(r'(?<!^)(?=[A-Z])', '_', i).lower())
+print(cc)
+ 
