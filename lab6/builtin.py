db4d8ad58cc9a4c1eff66f7b4ecb1c0f28a0ca3a
Batyrzhan <batyrzhan0607@gmail.com>
2025-03-02 20:00:59 +0500

    lab6

--- lab6/builtin.py
+++ lab6/builtin.py
@@ -9,36 +9,36 @@ print(multiplyList(lst))
 
 
 #2
-# def uplownum(s):
-#     up = 0
-#     low = 0
-#     for i in s:
-#         if ord(i) > 96:   # ord(i) возвращает ASCII-код символа i.
-#             low+=1
-#         else:
-#             up += 1
-#     return up,low
-# s = str(input("Word: "))
-# print(uplownum(s))
+def uplownum(s):
+    up = 0
+    low = 0
+    for i in s:
+        if ord(i) > 96:   # ord(i) возвращает ASCII-код символа i.
+            low+=1
+        else:
+            up += 1
+    return up,low
+s = str(input("Word: "))
+print(uplownum(s))
 
 
 #3
-# def isPolindrom(s):
-#     return s == s[::-1]
-# print(isPolindrom("madam"))
+def isPolindrom(s):
+    return s == s[::-1]
+print(isPolindrom("madam"))
 
 
 #4
-# import time
-# def squar(n, t):
-#     time.sleep(t/1000)
-#     return n**0.5
-# print(squar(25100, 2123))
+import time
+def squar(n, t):
+    time.sleep(t/1000)
+    return n**0.5
+print(squar(25100, 2123))
 
 
 #5
-# def allTrue(t):
-#     return all(t)
-# print(allTrue((1,1,5.5)))
+def allTrue(t):
+    return all(t)
+print(allTrue((1,1,5.5)))
 
 
