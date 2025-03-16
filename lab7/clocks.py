dc81a8c2271bc1cb924c51a888e61131f779ac47
Batyrzhan <batyrzhan0607@gmail.com>
2025-03-17 01:45:58 +0500

    lab7

---
+++ lab7/clocks.py
@@ -0,0 +1,44 @@
+import pygame as pg
+import datetime as dt
+
+pg.init()
+
+window = (800,600)
+
+clock = pg.time.Clock()
+screen = pg.display.set_mode(window) # 
+bg = pg.image.load("clockimg/clock.png") 
+
+min_hand = pg.image.load("clockimg/min_hand.png") #
+sec_hand = pg.image.load("clockimg/sec_hand.png") #
+
+def rotate(surf, img, times, angle):
+    rot_img = pg.transform.rotate(img, -(times % 60) * 6 + angle)
+    new_img = rot_img.get_rect(center = img.get_rect(center = (400, 300)).center)
+    print(new_img)
+    surf.blit(rot_img, new_img)
+
+
+pg.display.set_caption("Clock!")
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+    
+    
+    curtime = dt.datetime.now()
+    minuts = curtime.minute
+    seconds = curtime.second
+
+    screen.fill("BLACK")
+
+    screen.blit(bg, (0,0))
+    
+    rotate(screen, sec_hand, seconds, 60)
+    rotate(screen, min_hand, minuts, -45)
+
+    pg.display.update()
+    clock.tick(60)
+    
+pg.quit()
